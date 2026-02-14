2cac36b0886b69847ae5ff17e2ba5eff3062a8cf
sumitb252 <2025.sumit.bhatia@ves.ac.in>
2026-02-14 17:48:32 +0530

    Update and rename try.java to tryinput.java

--- tryinput.java
+++ tryinput.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class try{
+class tryinput{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter name:");
