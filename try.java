565e9f6cf5841abfd26a3f049056410e28d7abbc
sumitb252 <2025.sumit.bhatia@ves.ac.in>
2026-02-14 17:43:38 +0530

    Update try.java

--- try.java
+++ try.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class InputTest{
+class try{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter name:");
